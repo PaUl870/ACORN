bb13b0e23b2e0549ea39eb69f3e18eff09f89b90
Paul Bruegger <pbruegger@student.ethz.ch>
2024-11-11 12:19:49 +0100

    efs as param

--- build/_deps/nlohmann_json-build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /workspace/benchmark/code/ACORN/ACORN/build/_deps/nlohmann_json-src
-# Build directory: /workspace/benchmark/code/ACORN/ACORN/build/_deps/nlohmann_json-build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
